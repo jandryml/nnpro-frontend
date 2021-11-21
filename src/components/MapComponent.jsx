15a22d24a499f678cee225e2a4eb2fc074e9cecb
Jan Dryml <honza.dryml@icloud.com>
2021-11-22 00:52:48 +0100

    home map - add

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import {Map, PathLayer, Path, CompassControl, KeyboardControl, MouseControl, MarkerLayer, Marker} from 'react-mapycz'
 
-export const MapComponent = ({center, markers, trainPath}) => (
-    <Map width="1230px" height="600px" center={center} zoom={12}>
+export const MapComponent = ({center, markers, trainPath, separatePaths, zoom = 12, width = "1230px", height="600px"}) => (
+    <Map width={width} height={height} center={center} zoom={zoom}>
         <CompassControl/>
         <KeyboardControl/>
         <MouseControl zoom={true} wheel={true} pan={true}/>
         <PathLayer>
             {trainPath && <Path width={5} coords={trainPath} dynamicRoute={false} options={{color: "black"}} />}
+            {separatePaths && separatePaths.map((separatePath) => (
+                <Path key={separatePath.id} width={5} coords={separatePath} dynamicRoute={false} options={{color: "black"}} />
+                )
+            )}
         </PathLayer>
         <MarkerLayer>
             {markers.map((marker) => (
